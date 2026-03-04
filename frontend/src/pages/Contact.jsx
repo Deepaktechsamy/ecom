a8c9e2f4d93f130be5d097fcb382af0d606fb698
Deepak <deepaktechsol@gmail.com>
2026-03-04 13:43:25 +0530

    contact address name change

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,8 +15,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> T-nagar Chennai-600001</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: deepaktechsol@gmail.com</p>
+          <p className=' text-gray-500'>No.10, 1st Floor, 1st Cross Street, <br /> T-nagar Chennai-600001</p>
+          <p className=' text-gray-500'>Phone: (+91) 86673-76831 <br /> Email: deepaktechsol@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
